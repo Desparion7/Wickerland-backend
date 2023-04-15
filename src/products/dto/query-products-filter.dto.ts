f3fffe09ad7b4e42faa1c9c03237db58b4f94daf
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-04-15 16:46:00 +0200

    add filters for products

--- src/products/dto/query-products-filter.dto.ts
+++ src/products/dto/query-products-filter.dto.ts
@@ -1,12 +1,33 @@
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 export class ProductsFilterDto {
+  @IsOptional()
+  category: string;
+
+  @IsOptional()
+  subcategory: string;
+
+  @IsOptional()
+  search: string;
+
+  @IsOptional()
   @Transform(({ value }) => parseInt(value))
   @IsNumber()
   pageNumber: number;
 
+  @IsOptional()
   @Transform(({ value }) => parseInt(value))
   @IsNumber()
   pageSize: number;
+
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value))
+  @IsNumber()
+  min: number;
+
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value))
+  @IsNumber()
+  max: number;
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -10,15 +10,41 @@ export class ProductsService {
   async getProducts({
     pageNumber = 1,
     pageSize = 9,
+    min,
+    max,
+    category,
+    subcategory,
+    search,
   }): Promise<{ products: Product[]; totalPages: number }> {
-    const count = await this.productModel.countDocuments();
-    const totalPages = Math.ceil(count / pageSize);
+    // find all products
+    let query = this.productModel.find();
 
-    const products = await this.productModel
-      .find()
+    // filter when category
+    if (category !== 'undefined') {
+      query = query.where('category').in([category]);
+    }
+    // filter when subcategory
+    if (subcategory !== 'undefined') {
+      query = query.where('subcategory').in([subcategory]);
+    }
+
+    // filter products by price min to max
+    if (!isNaN(min) && !isNaN(max)) {
+      query = query.where('price').gte(min).lte(max);
+    }
+    // search products by name
+    if (search !== 'undefined') {
+      query = query.where('name', new RegExp(search, 'i'));
+    }
+
+    const products = await query
       .skip((pageNumber - 1) * pageSize)
       .limit(pageSize)
       .exec();
+
+    const count = await this.productModel.countDocuments(query.getQuery());
+    const totalPages = Math.ceil(count / pageSize);
+
     return { products, totalPages };
   }
 }
