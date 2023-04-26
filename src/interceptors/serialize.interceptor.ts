a1603a51f1b3c055b5f9bf7fca2cc3ed0d9a9300
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-04-26 14:26:12 +0200

    create interceptor serializer to expose only user email without password

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,27 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+import { UserDto } from 'src/users/dto/user.dto';
+
+export class SerializeInterceptor implements NestInterceptor {
+  intercept(
+    _context: ExecutionContext,
+    handler: CallHandler<any>,
+  ): Observable<any> {
+    // Run something before a request is handled by the request handler
+    return handler.handle().pipe(
+      map((data: any) => {
+        //  Run something before the response is sent out
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseInterceptors } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @UseInterceptors(SerializeInterceptor)
   @Post('/signup')
   async createUser(@Body() body: CreateUserDto) {
     const user = await this.usersService.signup(body.email, body.password);
